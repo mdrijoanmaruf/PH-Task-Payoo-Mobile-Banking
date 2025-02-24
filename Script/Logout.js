4f8782c20769d4a612dde24b1e8a756d16839f94
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-02-25 04:16:03 +0600

    Update path

--- Script/Logout.js
+++ Script/Logout.js
@@ -1,5 +1,5 @@
 document.getElementById('logoutBtn').addEventListener("click" , function(event){
     event.preventDefault();
     alert("Log out Successful")
-    window.location.href = "../index.html"
+    window.location.href = "index.html"
 })
